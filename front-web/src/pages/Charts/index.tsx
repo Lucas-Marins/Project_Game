5ec8dbfd0567a5416eb1e68a0345967dcd18f180
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-19 02:06:42 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -20,7 +20,7 @@ const initialPieData = {
     series:[]
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds-projeto-game.herokuapp.com'
 
 const Charts = () =>{
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -7,7 +7,7 @@ import Pagination from './Pagination';
 import {Link} from 'react-router-dom';
 import Filters from '../../components/Filters'
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds-projeto-game.herokuapp.com'
 
 const Records = () => {
 
